8f754c8e0986fe236fff39c8435c5b797603a1c2
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-05 17:44:39 +0300

    First Commit

---
+++ src/components/MovieList.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+
+const MovieList = (props) => {    // If a component is a stateless functional component, it can be written as a function.
+
+
+    return (    // Real Api TMDB Movie
+        <div className="row">
+
+
+            {props.movies.map((movie) => (
+                <div className="col-lg-4" key={movie.id}>
+                    <div className="card mb-4 shadow-sm">
+                        <img
+                            src = {"https://www.themoviedb.org/t/p/w1280" + movie.poster_path}
+                            className="card-img-top"
+                            alt={movie.title}
+                        />
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                {movie.title}
+                            </h5>
+                            <p className="card-text">
+                                {movie.overview}
+                            </p>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <h2>
+                                    <span className="badge badge-info">
+                                        {movie.vote_average}
+                                    </span>
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+
+        </div>
+    )
+}
+
+
+export default MovieList;
