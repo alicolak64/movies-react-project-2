f684e294d019c66feb7d2281ff742b721dcbb24a
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-05 19:04:09 +0300

    Bootsrap Fixed

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -12,21 +12,27 @@ const MovieList = (props) => {    // If a component is a stateless functional co
                 <div className="col-lg-4" key={movie.id}>
                     <div className="card mb-4 shadow-sm">
                         <img
-                            src = {"https://www.themoviedb.org/t/p/w1280" + movie.poster_path}
+                            src={"https://www.themoviedb.org/t/p/w1280" + movie.poster_path}
                             className="card-img-top"
                             alt={movie.title}
                         />
                         <div className="card-body">
-                            <h5 className="card-title">
+                            <h3 className="card-title">
                                 {movie.title}
-                            </h5>
+                            </h3>
                             <p className="card-text">
                                 {movie.overview}
                             </p>
                             <div className="d-flex justify-content-between align-items-center">
                                 <h2>
-                                    <span className="badge badge-info">
-                                        {movie.vote_average}
+                                    <span className="badge bg-secondary">
+                                        {movie.release_date}
+                                    </span>
+                                </h2>
+
+                                <h2>
+                                    <span className="badge bg-info">
+                                        {movie.vote_average % 1 === 0 ? movie.vote_average + ".0" : movie.vote_average}
                                     </span>
                                 </h2>
                             </div>
